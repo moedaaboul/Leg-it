198c494d7b3974a3d1298f7989a1d176af4bbad7
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-04 17:31:41 +0000

    change variables

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
-  cloud_name: 'dn0xeptfr',
-  api_key: '564649231227524',
-  api_secret: 'Qb-VHE8RXG3LI1D3KAcfqiyU5rE',
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = { cloudinary };
